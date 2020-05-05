1e582ebcc96116fbc402646e3580fb9f4e27297a
Bill Smart <smartw@oregonstate.edu>
2020-05-05 10:27:57 -0700

    Added map.

--- content/contact.php
+++ content/contact.php
@@ -1,62 +1,86 @@
 The best way to contact me is probably by <?php email('email'); ?>.  My office and lab space are both in <a target="_blank" href="https://www.google.com/maps/place/Graf+Hall,+1680+SW+Monroe+Ave,+Corvallis,+OR+97331">Graf Hall</a>.  If I'm on campus, that's where I'm likely to be.
 
-<dl>
-	<p>
-		<dt><b>Office:</b></dt>
-		<dd>
-			<a target="_blank" href=href="https://www.google.com/maps/place/Graf+Hall,+1680+SW+Monroe+Ave,+Corvallis,+OR+97331">316 Graf Hall</a>
-		</dd>
-	</p>
+<style>
+* {
+  box-sizing: border-box;
+}
 
-	<p>
-		<dt><b>Office Hours:</b></dt>
-		<dd>
-			Wednesdays, 1pm to 2:30pm.<br><a target="_blank" href="http://tinyurl.com/SmartOfficeHours">Reserve a slot</a> or <?php email('email me') ?> to set up another time.
-		</dd>
-	</p>
+/* Create two equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 50%;
+  padding: 10px;
+}
 
-	<p>
-		<dt><b>Lab:</b></dt>
-		<dd>
-			<a target="_blank" href="https://www.google.com/maps/place/Graf+Hall,+1680+SW+Monroe+Ave,+Corvallis,+OR+97331">203 Graf Hall</a>		
-		</dd>
-	</p>
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
 
-	<p>
-		<dt><b>Mailing Address:</b></dt>
-		<dd>
-			Bill Smart<br>
-			<a target="_blank" href="http://oregonstate.edu">Oregon State University</a><br>
-			<a target="_blank" href="https://www.google.com/maps/place/2000+NW+Monroe+Ave+Rogers+Hall,+Corvallis,+OR+97331">204 Rogers Hall<a/><br>
-			Corvallis, OR 97331<br>
-			United States<br>
-		</dd>
-	</p>
 
-	<p>
-		<dt><b>Phones:</b></dt>
-		<dd>
-			Phone: <a href="tel:5418380670">+1 (541) 737-0670</a><br>
-			Fax: <a href="tel:5419082600">+1 (541) 737-2600</a><br>
-		</dd>
-	</p>
+<div class="row">
+	<div class="column">
+		<dl>
+			<p>
+				<dt><b>Office:</b></dt>
+				<dd>
+					<a target="_blank" href=href="https://www.google.com/maps/place/Graf+Hall,+1680+SW+Monroe+Ave,+Corvallis,+OR+97331">316 Graf Hall</a>
+				</dd>
+			</p>
 
-	<p>
-		<dt><b>Electronic:</b></dt>
-		<dd>
-			Email: <?php email(); ?><br>
-			Skype: <a href="skype:wdsmart">wdsmart</a><br>
-			Google Hangouts: smartw@oregonstate.edu<br>
-			LinkedIn: <a target="_blank" href="http://www.linkedin.com/in/wdsmart">wdsmart</a><br>
-			Twitter: <a target="_blank" href="http://twitter.com/wdsrobot">wdsrobot</a><br>
-		</dd>
-	</p>
+			<p>
+				<dt><b>Office Hours:</b></dt>
+				<dd>
+					Wednesdays, 1pm to 2:30pm.<br><a target="_blank" href="http://tinyurl.com/SmartOfficeHours">Reserve a slot</a> or <?php email('email me') ?> to set up another time.
+				</dd>
+			</p>
 
-	<p>
-		<dt><b></b></dt>
-		<dd>
-			
-		</dd>
-	</p>
+			<p>
+				<dt><b>Lab:</b></dt>
+				<dd>
+					<a target="_blank" href="https://www.google.com/maps/place/Graf+Hall,+1680+SW+Monroe+Ave,+Corvallis,+OR+97331">203 Graf Hall</a>	
+				</dd>
+			</p>
 
-</dl>
+			<p>
+				<dt><b>Mailing Address:</b></dt>
+				<dd>
+					Bill Smart<br>
+					<a target="_blank" href="http://oregonstate.edu">Oregon State University</a><br>
+					<a target="_blank" href="https://www.google.com/maps/place/2000+NW+Monroe+Ave+Rogers+Hall,+Corvallis,+OR+97331">204 Rogers Hall<a/><br>
+					Corvallis, OR 97331<br>
+					United States<br>
+				</dd>
+			</p>
+
+			<p>
+				<dt><b>Phones:</b></dt>
+				<dd>
+					Phone: <a href="tel:5418380670">+1 (541) 737-0670</a><br>
+					Fax: <a href="tel:5419082600">+1 (541) 737-2600</a><br>
+				</dd>
+			</p>
+
+			<p>
+				<dt><b>Electronic:</b></dt>
+				<dd>
+					Email: <?php email(); ?><br>
+					Skype: <a href="skype:wdsmart">wdsmart</a><br>
+					Google Hangouts: smartw@oregonstate.edu<br>
+					LinkedIn: <a target="_blank" href="http://www.linkedin.com/in/wdsmart">wdsmart</a><br>
+					Twitter: <a target="_blank" href="http://twitter.com/wdsrobot">wdsrobot</a><br>
+				</dd>
+			</p>
+		</dl>
+	</div>
+
+	<div class="column">
+		<p>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2842.4352894591025!2d-123.27706714845014!3d44.567661578998056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54c040bdc4461ea9%3A0x981a3b57c9919466!2sGraf%20Hall%2C%201680%20SW%20Monroe%20Ave%2C%20Corvallis%2C%20OR%2097331!5e0!3m2!1sen!2sus!4v1588697863580!5m2!1sen!2sus" width="100%" height="200%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		</p>
+	</div>
+</div>
+
